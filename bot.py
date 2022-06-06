69d6d4443d5b1351711e4699d967d2a98cc32846
Alex Zhur <zhur06@gmail.com>
2022-06-06 16:07:38 +0300

    Добавлена настройка 'Простой Режим' , переорганизован обработчик сообщений, добавлена краткая справка по коммандам

--- bot.py
+++ bot.py
@@ -1,3 +1,4 @@
+from pyparsing import Each
 import config
 import logging
 import main
@@ -17,6 +18,8 @@ dp = Dispatcher(bot)
 # async def echo(message: types.Message):
 #     await message.answer(message.text)
 
+selected_song = ''
+easyMode = True
 
 @dp.message_handler(commands = ['start'])
 async def echo(message: types.Message):
@@ -25,21 +28,63 @@ async def echo(message: types.Message):
 @dp.message_handler(commands = ['song_text'])
 async def song_text(message: types.Message):
     try:
-        await message.answer(main.get_song_info_1(message.get_args()) + '\n' + main.get_song_info_2(message.get_args()))
-        await message.answer(main.get_song(message.get_args()))
+        if message.get_args() != '':
+            await message.answer(main.get_song_info_1(message.get_args()) + '\n' + main.get_song_info_2(message.get_args()))
+            await message.answer(main.get_song(message.get_args()))
+        else:
+            await message.answer(main.get_song_info_1(selected_song) + '\n' + main.get_song_info_2(selected_song))
+            await message.answer(main.get_song(selected_song))
     except AttributeError:
         await message.reply('Такой песни не найдено :(')
 
 @dp.message_handler()
 async def echo(message: types.Message):
+    global easyMode
+    global selected_song
+    # Навигация в меню
     if message.text == commands.mainMenu:
         await bot.send_message(message.from_user.id, random.choice(commands.Messages), reply_markup = nav.mainMenu)
 
     elif message.text == commands.otherMenu:
         await bot.send_message(message.from_user.id, random.choice(commands.Messages), reply_markup = nav.otherMenu)
+    
+    elif message.text == commands.configs:
+        await bot.send_message(message.from_user.id, 'Ваши настройки: ', reply_markup = nav.configMenu)
+        replyText = 'Простой режим: '
+        if easyMode:
+            replyText += 'Включён'
+        else:
+            replyText += 'Выключен'
+        await message.answer(replyText)
 
+    elif message.text == commands.commandsInfoMenu:
+        await bot.send_message(message.from_user.id, 'Для получения справки по коммандам выберите соответствующую на клавиатуре', reply_markup = nav.commandsInfoMenu)
+
+    # Комманды бота
     elif message.text == commands.song_text:
-        await message.answer('Введите команду /song_text [Название песни]')
+        if easyMode:
+            await message.answer('Напишите название песни а затем /song_text')
+        else:
+            try:
+                await message.answer(main.get_song_info_1(selected_song) + '\n' + main.get_song_info_2(selected_song))
+                await message.answer(main.get_song(selected_song))
+            except AttributeError:
+                await message.reply('Такой песни не найдено :(')
+
+    elif message.text == commands.easyMode:
+        await message.answer(random.choice(commands.Messages))
+        easyMode = not easyMode
+
+    # Комманды справки
+    elif message.text == commands.song_text + '?':
+        await message.answer('Отправляет сводку по песне (полное название, автора) и её текст с аккордами для гитары')
+
+    elif message.text == commands.easyMode + '?':
+        await message.answer('Простой режим, отвечает за реакции бота: поясняет ли бот что нужно сделать при использовании комманд (Простой режим включён) или просто выполняет их со стандартными настройками (Простой режим выключен)')
+
+    # Обработчик всех сообщений
+    else:
+        selected_song = message.text
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates='True')
--- markups.py
+++ markups.py
@@ -3,11 +3,22 @@ import commands
 
 btnMain = KeyboardButton(commands.mainMenu)
 
-# MainMenu
+# Main Menu
 btnSong_text = KeyboardButton(commands.song_text)
 btnOther = KeyboardButton(commands.otherMenu)
 mainMenu = ReplyKeyboardMarkup(resize_keyboard = True).add(btnSong_text, btnOther)
 
-# Other Meny
+# Other Menu
 btnInfo = KeyboardButton(commands.info)
-otherMenu = ReplyKeyboardMarkup(resize_keyboard = True).add(btnInfo, btnMain)
+btnConfigs = KeyboardButton(commands.configs)
+btnCommandsInfo = KeyboardButton(commands.commandsInfoMenu)
+otherMenu = ReplyKeyboardMarkup(resize_keyboard = True).add(btnInfo, btnConfigs, btnCommandsInfo, btnMain)
+
+# Config Menu
+btnEasyMode = KeyboardButton(commands.easyMode)
+configMenu = ReplyKeyboardMarkup(resize_keyboard = True).add(btnEasyMode, btnMain)
+
+# Commands Info Menu 
+btnSong_textInfo = KeyboardButton(commands.song_text + '?')
+btnEasyModeInfo = KeyboardButton(commands.easyMode + '?')
+commandsInfoMenu = ReplyKeyboardMarkup(resize_keyboard = True).add(btnSong_textInfo, btnEasyModeInfo, btnMain)
